1e667343bee6c23e55218b15df0754ca34b8c7df
superzhc <zhengchao0555@163.com>
2023-04-26 18:04:11 +0800

    c++ update

---
+++ superz-learning/superz-learning-cpp/main2.cpp
@@ -0,0 +1,11 @@
+//
+// Created by superz on 2023/4/26.
+//
+#include <iostream>
+
+//using namespace std;
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
---
+++ superz-learning/superz-learning-cpp/string.cpp
@@ -0,0 +1,11 @@
+//
+// Created by superz on 2023/4/26.
+//
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string str = "hello world";
+    std::cout << str << std::endl;
+    return 0;
+}
