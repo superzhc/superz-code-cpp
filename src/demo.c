bd5e3f59e588ab0ad1aef21b899e21e3f278100a
superzhc <zhengchao0555@163.com>
2023-07-28 18:01:53 +0800

    init project

---
+++ src/demo.c
@@ -0,0 +1,8 @@
+//
+// Created by root on 2023/7/28.
+//
+#include <stdio.h>
+
+int main(void){
+    printf("Hello World");
+}
--- superz-learning/superz-learning-cpp/main.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by superz on 2023/4/22.
-//
-//#include "stdio.h"
-
-int main() {
-    return 0;
-}
